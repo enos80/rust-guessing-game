d2976bed7c895a15646c159705b59d4410590aa0
Daniel Enos <enos80@protonmail.com>
2022-02-14 15:58:16 -0600

    Updated version and added comparison code.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering
 
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("The secret number is: {}", secret_number)
+
 
     println!("Guess the number!");
     println!("Enter your guess!!");
 
     let mut guess = String::new();
+    let guess: u32 = guess.trim().parse().expect("Please type a number!")
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
     println!("You guessed: {}", guess);
-    println!("The secret number is: {}", secret_number)
-
+    match guess.cmpp(&secret_number) {
+        Ordering::Less => println!("Too Small"),
+        Ordering::Greater => println!("Too Large"),
+        Ordering::Equal => println!("Bingo!! You win!")
+    }
 }
